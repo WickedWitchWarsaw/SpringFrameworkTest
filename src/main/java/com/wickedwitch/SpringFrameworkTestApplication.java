2df19596119c81d08cedd165a7e866adadda2622
WickedWitchWarsaw <zuzka.pacak@gmail.com>
2016-12-17 14:26:59 +0100

    Initial commit

---
+++ src/main/java/com/wickedwitch/SpringFrameworkTestApplication.java
@@ -0,0 +1,12 @@
+package com.wickedwitch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkTestApplication.class, args);
+	}
+}
