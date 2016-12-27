8f86c85c1419d792e87a34d7cb6750947897b017
WickedWitchWarsaw <zuzka.pacak@gmail.com>
2016-12-27 10:14:37 +0100

    SLF4J Logger added & Language property messages - LocaleResolver

--- src/main/java/com/wickedwitch/config/I18NConfig.java
+++ src/main/java/com/wickedwitch/config/I18NConfig.java
@@ -3,13 +3,19 @@ package com.wickedwitch.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
 
 /**
  * Created by ZuZ on 2016-12-26.
  */
 
 @Configuration
-public class I18nConfig {
+public class I18NConfig {
 
     @Bean
     public ReloadableResourceBundleMessageSource messageSource() {
@@ -17,5 +23,16 @@ public class I18nConfig {
         bundleMessageSource.setBasename("classpath:i18n/messages");
         bundleMessageSource.setCacheSeconds(1800);
         return bundleMessageSource;
+    }
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        SessionLocaleResolver slr = new SessionLocaleResolver();
+
+        // Locale french = new Locale("fr", "FR");
+        // slr.setDefaultLocale(french);
+
+        slr.setDefaultLocale(Locale.UK);
+        return slr;
     }
 }
--- src/main/java/com/wickedwitch/controller/PersonController.java
+++ src/main/java/com/wickedwitch/controller/PersonController.java
@@ -1,11 +1,15 @@
 package com.wickedwitch.controller;
 
 import com.wickedwitch.pojo.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.springframework.boot.Banner.Mode.LOG;
 
 /**
  * Created by ZuZ on 2016-12-25.
@@ -14,21 +18,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class PersonController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(PersonController.class);
+
     @RequestMapping("/form")
-    public String showForm(Model model){
+    public String showForm(Model model) {
 
         Person person = new Person();
-        person.getFirstName();
-        person.getLastName();
-        person.getEmail();
-
         model.addAttribute("person", person);
         return "form";
     }
 
     @RequestMapping(value = "/form", method = RequestMethod.POST)
-    public void formPost(@ModelAttribute ("person") Person person){
-        System.out.println(person);
-    }
+    public void formPost(@ModelAttribute("person") Person person) {
+        LOG.info("Adding person details: {}", person);
 
+    }
 }
